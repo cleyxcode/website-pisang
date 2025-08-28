4e6f6b09f090d704246b320b14ef58856b054732
cleyxcode <afiafikadewi3@gmail.com>
2025-08-28 13:08:13 +0900

    update Seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -12,23 +13,29 @@ class CategorySeeder extends Seeder
         $categories = [
             [
                 'name' => 'Keripik Pisang',
+                'slug' => Str::slug('Keripik Pisang'),
                 'description' => 'Berbagai varian keripik pisang crispy dan gurih',
                 'is_active' => true,
             ],
             [
                 'name' => 'Snack Asin',
+                'slug' => Str::slug('Snack Asin'),
                 'description' => 'Camilan asin yang cocok untuk teman minum teh atau kopi',
                 'is_active' => true,
             ],
             [
                 'name' => 'Snack Manis',
+                'slug' => Str::slug('Snack Manis'),
                 'description' => 'Camilan manis untuk menghilangkan stress dan mood booster',
                 'is_active' => true,
             ],
         ];
 
         foreach ($categories as $category) {
-            Category::create($category);
+            Category::updateOrCreate(
+                ['slug' => $category['slug']], // cek dulu biar tidak duplikat
+                $category
+            );
         }
     }
 }
